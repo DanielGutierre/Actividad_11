76fbc71795cb22c4fc2c2591595299c4b03be266
DanielGutierre <earody4@gmail.com>
2022-06-06 18:34:15 -0600

    Primer Version

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  const getMovies = async () => {
+    
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+      console.log(json)
+    } catch (error) {
+      console.error(error);
+    
+    }
+  }
+ 
+
+
+
+ useEffect(() => {
+  getMovies();
+}, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}</Text>
+          )}
+        />
     </View>
   );
 }
